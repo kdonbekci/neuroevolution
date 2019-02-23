0c3052501737826c8e3438405c9995ece9ae88b6
kdonbekci <kdonbekci@gmail.com>
2019-02-22 17:41:43 -0800

    copying functions

--- src/config.py
+++ src/config.py
@@ -1,4 +1,10 @@
 class Configuration:
     INITIAL_POPULATION_SIZE = 100
     
-    MODEL_FLOAT_PRECISION='float16'
+    MODEL_FLOAT_PRECISION = 'float16'
+    
+    DEFAULT_NODE_ACTIVATION = 'linear'
+    
+    DEFAULT_NODE_EXPRESSED = True
+    
+    DEFAULT_NODE_BIAS = 0.0
