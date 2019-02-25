65f670638ae4a24ab9f709a085df7076bbf19478
kdonbekci <kdonbekci@gmail.com>
2019-02-24 17:45:41 -0800

    trying to rename

--- src/Phylogeny.py
+++
@@ -1,5 +0,0 @@
-from ete3 import Tree
-
-class Phylogeny:
-    def __init__(self):
-        self.tree = Tree()
--- src/notebooks/importer.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-from pathlib import Path
-my_path = Path().absolute()
-src_path = os.path.join(my_path, os.pardir)
-sys.path.append(src_path)
