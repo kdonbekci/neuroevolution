30a58f01917ca6e7e0fe5336ff82be921542dfbd
kdonbekci <kdonbekci@gmail.com>
2019-02-27 18:02:06 -0800

    moved notebooks

--- notebooks/importer.py
+++ notebooks/importer.py
@@ -2,5 +2,5 @@ import sys
 import os
 from pathlib import Path
 my_path = Path().absolute()
-src_path = os.path.join(my_path, os.pardir)
+src_path = os.path.join(my_path, 'src')
 sys.path.append(src_path)
